8de07b9adcc82d23fc45d0a6a01fe1304c0b1904
wangshaoshuai <wangshaoshuai@wangshaoshuaideMacBook-Pro.local>
2018-11-29 23:30:41 +0800

    add

--- src/main/java/com/example/demo/mode/GoodsRecord.java
+++ src/main/java/com/example/demo/mode/GoodsRecord.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -17,6 +18,7 @@ public class GoodsRecord {
 
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
+    @GenericGenerator(name = "native", strategy = "native")
     @Column(name="id")
     private long id;
 
